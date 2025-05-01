7d4b07a48b4b868dbee5ab2b39910a01b9f5a471
ChungWoo Lee <canwooj@gmail.com>
2025-05-02 05:14:03 +0900

    Add and integrate undergraduate research summary PDF into devlog post

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -1,16 +1,22 @@
+// components/pdf-viewer.tsx
 import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+
 const PdfViewer = ({ fileUrl }: { fileUrl: string }) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin()
 
   return (
     <div style={{ height: '750px' }}>
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl={`${basePath}${fileUrl}`}
+          plugins={[defaultLayoutPluginInstance]}
+        />
       </Worker>
     </div>
   )
